1f6815e641b2924166a2293b16c606c338c5ef7e
Andrew <andrewmcdan@gmail.com>
2023-09-10 17:31:49 -0400

    ..2

--- FeatherM0_RFM9x/FeatherM0_RFM9x.ino
+++ FeatherM0_RFM9x/FeatherM0_RFM9x.ino
@@ -381,6 +381,8 @@ void loop() {
         }
     }
 
+
+
     // send a beacon every 60 seconds
     if(millis() - timeSinceLastBeacon > 60 * 1000){
         debugSerial.println("Sending beacon...");
